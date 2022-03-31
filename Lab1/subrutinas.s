c29b2cc8ab8ef463b79e3a60f5d208e9b19e1f69
carlogs99 <carlosgruss99@gmail.com>
2022-03-31 11:55:24 -0300

    Added comments to subrutinas.s, added gbd debugging code in initialize_clk.txt

--- Lab1/subrutinas.s
+++ Lab1/subrutinas.s
@@ -36,11 +36,11 @@ hexa7seg:
 	ret 
 
 pbcda7seg:
-	ld b, a
+	ld b, a			; preservar el contenido del acumulador
 	call hexa7seg
 	ld c, a
-	ld a, b
-	sra a
+	ld a, b			; restaurar el acumulador original
+	sra a			; rotar cuatro posiciones hacia la derecha
 	sra a
 	sra a
 	sra a
@@ -52,12 +52,12 @@ binapbcd:
 	ld b, 0d
 	loop_binapbcd: 
 		cp 10d
-		jp m, fin_binapbcd
-		sub 10d
+		jp m, fin_binapbcd	; if (A - 10d) > 0 then jump a fin_binapbcd
+		sub 10d				; else restar 10 e incrementar contador
 		inc b
 		jp loop_binapbcd
 	fin_binapbcd: 
-		sla b
+		sla b				; rotar cuatro posiciones a la izquierda
 		sla b
 		sla b
 		sla b
@@ -65,58 +65,58 @@ binapbcd:
 	ret
 
 despreloj:
-	ld a, (ix)
+	ld a, (ix)				; cargamos los segundos al acumulador
 	call binapbcd
-	call pbcda7seg
-	ld a, b
-	out (HEX3), a
+	call pbcda7seg			; en bc tenemos los segundos codificados a 7seg
+	ld a, b			
+	out (HEX3), a	
 	ld a, c
-	ld e, a
-	out (HEX2), a
+	ld e, a					; preservamos lo que va en HEX2
+	out (HEX2), a			; por si hay que agregarle punto decimal
 	inc ix
-	ld a, (ix)
+	ld a, (ix)				; cargamos las centesimas al acumulador
 	call binapbcd
-	call pbcda7seg
+	call pbcda7seg			; en bc tenemos las centesimas codificadas a 7seg
 	ld a, b
 	out (HEX1), a
 	ld a, c
 	out (HEX0), a
-	inc ix
-	ld a, (ix)
-	cp 0d
-	jp nz, fin_despreloj
-	ld a, e
-	and 01111111B
+	inc ix			
+	ld a, (ix)		
+	cp 0d					; evalua como cero solo si la flag es 0x00
+	jp nz, fin_despreloj	; si no vale 0x00, vale 0xFF y no se debe agregar punto decimal
+	ld a, e					; recuperamos lo que va en HEX2
+	and 01111111B			; mascara para prender el punto decimal
 	out (HEX2), a
 	fin_despreloj: 
-	dec ix
+	dec ix					; restauramos el registro ix
 	dec ix
 	ret
 	
 decreloj:
-	ld b,(ix)
-	ld a,(ix+1)
-	cp 10d
-	jp m, cent_neg
-	sub 10d
+	ld b,(ix) ; cargar los segundos
+	ld a,(ix+1)	; cargar las centesimas
+	cp 10d		
+	jp m, cent_neg	; centesimas-10 >= 0? (underflow centesimas)
+	sub 10d			; si no hay underflow, restamos 10
 	ld (ix+1), a
-	jp fin_decreloj
-	cent_neg: 
-		ld c,(ix+1)
+	jp fin_decreloj	; y saltamos al final
+	cent_neg: 		; si hay underflow en las centesimas:
+		ld c,(ix+1)	
 		ld a,b
-		cp 1d
-		jp m , seg_neg
-		sub 1d
+		cp 1d		
+		jp m , seg_neg	; segundos-1 >= 0? (underflow segundos)
+		sub 1d			; si no hay, restamos 1
 		ld (ix), a
 		ld a, c
-		add a, 90d
+		add a, 90d		; las centesimas valen 90+resto previo
 		ld (ix+1), a
 		ld a, (ix+2)
-		cpl
+		cpl				; complementar la bandera
 		ld (ix+2), a
 		jp fin_decreloj
-	seg_neg:
-		ld (ix), 0d
+	seg_neg:			; si hay underflow en los segundos:
+		ld (ix), 0d		; satura a cero el contador
 		ld (ix+1), 0d
 	fin_decreloj:
 	ret
